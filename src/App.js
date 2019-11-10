3be3f807499190991e364b81f73438c30864782c
vic <victordjohnson@gmail.com>
2019-11-10 16:37:57 -0600

    added quite a bit. we now have a delta function to measure distance between colors (not used yet) and a grid to display the colors found

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from "react";
 import "./App.css";
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.canvasRef = React.createRef();
-  }
+import Color from "./Color";
 
-  onChange = e => {
+const App = () => {
+  const [uImage, setUImage] = React.useState("");
+  const [colors, setColors] = React.useState([]);
+  const canvasRef = React.createRef();
+
+  const onChange = e => {
     if (!e.target.files.length) return;
 
     var imgSrc = URL.createObjectURL(e.target.files[0]);
-    var canvas = this.canvasRef.current;
+    setUImage(imgSrc);
+    var canvas = canvasRef.current;
     var ctx = canvas.getContext("2d");
     var img = new Image();
 
@@ -22,32 +24,61 @@ class App extends React.Component {
     }
 
     function pixelate() {
-      canvas.height = img.height;
-      canvas.width = img.width;
+      var size = 4;
 
-      var widthSize = 4 / img.width;
-      var heightSize = 4 / img.height;
+      var height = (canvas.height = img.height);
+      var width = (canvas.width = img.width);
 
-      var w = canvas.width * widthSize;
-      var h = canvas.height * heightSize;
+      var widthSize = size / width;
+      var heightSize = size / height;
+
+      var w = width * widthSize;
+      var h = height * heightSize;
 
       ctx.drawImage(img, 0, 0, w, h);
-
       ctx.mozImageSmoothingEnabled = false;
       ctx.imageSmoothingEnabled = false;
-      ctx.drawImage(canvas, 0, 0, w, h, 0, 0, canvas.width, canvas.height);
+      ctx.drawImage(canvas, 0, 0, w, h, 0, 0, width, height);
+
+      let colorArr = [];
+      for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+          var c = ctx.getImageData(
+            (width / size) * i + width / (size * 2),
+            (height / size) * j + height / (size * 2),
+            1,
+            1
+          ).data;
+
+          var color = {
+            r: c[0],
+            g: c[1],
+            b: c[2]
+          };
+
+          colorArr.push(color);
+        }
+      }
+
+      setColors(
+        colorArr.map(x => (
+          <Color key={`${x.r}${x.g}${x.b}`} r={x.r} g={x.g} b={x.b} />
+        ))
+      );
     }
     draw();
   };
 
-  render() {
-    return (
-      <div className="container">
-        <canvas ref={this.canvasRef}></canvas>
-        <input type="file" name="file-input" onChange={this.onChange} />
+  return (
+    <div className="container">
+      <div className="photo-contianer">
+        <canvas ref={canvasRef}></canvas>
+        <img src={uImage} alt="" />
+        <input type="file" name="file-input" onChange={onChange} />
       </div>
-    );
-  }
-}
+      <div className="results">{colors}</div>
+    </div>
+  );
+};
 
 export default App;
