205e41dbc72bf1a3de12270723a63558a40f9ec0
vic <victordjohnson@gmail.com>
2019-11-09 21:14:46 -0600

    adjusted how the original image is scaled down but would like to change the size of the file to be dynamically adjusted

--- src/App.js
+++ src/App.js
@@ -6,59 +6,57 @@ class App extends React.Component {
     super();
     this.canvasRef = React.createRef();
   }
-  onSubmit = e => {
-    e.preventDefault();
-  };
 
   onChange = e => {
-    var imgSrc = URL.createObjectURL(e.target.files[0]);
+    if (!e.target.files.length) return;
 
-    // Grab the Canvas and Drawing Context
+    var imgSrc = URL.createObjectURL(e.target.files[0]);
     var canvas = this.canvasRef.current;
     var ctx = canvas.getContext("2d");
-
-    // Create an image element
     var img = new Image();
 
-    //takes any image URL and creates an un pixelated image /4 the orginal size of the image
     function draw(imgURL) {
-      // Specify the src to load the image
       img.crossOrigin = "anonymous";
       img.src = imgURL;
-
-      img.onload = function() {
-        if (img.height > 500) {
-          const aspectRatio = img.height / img.width;
-          img.height = 500;
-          img.width = 500 / aspectRatio;
-        }
-
+      img.onload = () => {
         canvas.height = img.height / 4;
         canvas.width = img.width / 4;
-        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+
         pixelate();
       };
     }
 
     //
     function pixelate() {
-      //dynamically adjust canvas size to the size of the uploaded image
       canvas.height = img.height;
       canvas.width = img.width;
 
-      /// if in play mode use that value, else use slider value
-      var size = 0.007; // blocks.value * 0.01,
-      /// cache scaled width and height
+      const area = img.height * img.width;
+
+      var size = 0.007;
+
+      if (area > 12000000) {
+        size = 0.001;
+      } else if (area > 7000000) {
+        size = 0.0015;
+      } else if (area > 5000000) {
+        size = 0.002;
+      } else if (area > 1500000) {
+        size = 0.00225;
+      } else if (area > 900000) {
+        size = 0.0025;
+      } else if (area > 350000) {
+        size = 0.004;
+      }
+
       var w = canvas.width * size;
       var h = canvas.height * size;
 
-      /// draw original image to the scaled size
       ctx.drawImage(img, 0, 0, w, h);
 
-      /// then draw that scaled image thumb back to fill canvas
-      /// As smoothing is off the result will be pixelated
       ctx.mozImageSmoothingEnabled = false;
       ctx.imageSmoothingEnabled = false;
+
       ctx.drawImage(canvas, 0, 0, w, h, 0, 0, canvas.width, canvas.height);
     }
     draw(imgSrc);
@@ -67,14 +65,8 @@ class App extends React.Component {
   render() {
     return (
       <div className="container">
-        <canvas ref={this.canvasRef} width="200" height="100"></canvas>
-        <img src={""} alt="" />
-        <form className="form" onSubmit={this.onSubmit}>
-          <div>
-            <input type="file" name="file-input" onChange={this.onChange} />
-            <button type="submit">Submit</button>
-          </div>
-        </form>
+        <canvas ref={this.canvasRef}></canvas>
+        <input type="file" name="file-input" onChange={this.onChange} />
       </div>
     );
   }
