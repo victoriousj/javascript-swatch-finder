6d525fa52450c582d975c796ee2a94ec8d007e02
vic <victordjohnson@gmail.com>
2019-11-11 11:30:28 -0600

    Updates

---
+++ precache-manifest.ed36b1115a688267f6615287a65af620.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30ee0bb2f12e2fdc597db70ba0142df2",
+    "url": "/javascript-swatch-finder/index.html"
+  },
+  {
+    "revision": "ce37b6daef162188cc6c",
+    "url": "/javascript-swatch-finder/static/css/main.1aef4ba2.chunk.css"
+  },
+  {
+    "revision": "2b13a14973fb999c5161",
+    "url": "/javascript-swatch-finder/static/js/2.d56e569f.chunk.js"
+  },
+  {
+    "revision": "ce37b6daef162188cc6c",
+    "url": "/javascript-swatch-finder/static/js/main.58875b19.chunk.js"
+  },
+  {
+    "revision": "42cce5296eb96a6df142",
+    "url": "/javascript-swatch-finder/static/js/runtime-main.721c087d.js"
+  },
+  {
+    "revision": "3a68361728d10c4d211bfaf893a09d67",
+    "url": "/javascript-swatch-finder/static/media/swatch-finder.3a683617.svg"
+  }
+]);
