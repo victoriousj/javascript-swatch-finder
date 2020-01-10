55453af09e2b5874c3ddea3f7e31acad2129af42
vic <victordjohnson@gmail.com>
2020-01-10 12:00:01 -0600

    Updates

---
+++ precache-manifest.9d65aa4c28456b56bcf055d35eacd74e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa8ca54b688dd385f15610608a5d4589",
+    "url": "/javascript-swatch-finder/index.html"
+  },
+  {
+    "revision": "44c2b22d07f5a521ea4e",
+    "url": "/javascript-swatch-finder/static/css/main.67dbe3ee.chunk.css"
+  },
+  {
+    "revision": "1c64268638f989728fe5",
+    "url": "/javascript-swatch-finder/static/js/2.c41b2126.chunk.js"
+  },
+  {
+    "revision": "e92e356b467ce5268290e03394dbf217",
+    "url": "/javascript-swatch-finder/static/js/2.c41b2126.chunk.js.LICENSE"
+  },
+  {
+    "revision": "44c2b22d07f5a521ea4e",
+    "url": "/javascript-swatch-finder/static/js/main.3d2ce007.chunk.js"
+  },
+  {
+    "revision": "3d7f9bdbf2fd2bdc1b7a",
+    "url": "/javascript-swatch-finder/static/js/runtime-main.2c9a1bea.js"
+  },
+  {
+    "revision": "3a68361728d10c4d211bfaf893a09d67",
+    "url": "/javascript-swatch-finder/static/media/swatch-finder.3a683617.svg"
+  },
+  {
+    "revision": "51b277279c14c31918239ec48733a591",
+    "url": "/javascript-swatch-finder/static/media/upload.51b27727.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/javascript-swatch-finder/precache-manifest.47947edda6411e81fc15f33a7e05231e.js"
+  "/javascript-swatch-finder/precache-manifest.9d65aa4c28456b56bcf055d35eacd74e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/javascript-swatch-finder/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
