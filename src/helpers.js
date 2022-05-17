633cc3424c1e4b4e445a4826e75854900ca7c4a1
Victor Johnson <vic@victordjohnson.com>
2022-05-16 22:29:47 -0500

    styling

--- src/helpers.js
+++ src/helpers.js
@@ -16,18 +16,22 @@ export const colorDelta = (hex1, hex2) => {
     var r1 = parseInt(hex1.substring(0, 2), 16);
     var g1 = parseInt(hex1.substring(2, 4), 16);
     var b1 = parseInt(hex1.substring(4, 6), 16);
+
     // get red/green/blue int values of hex2
     var r2 = parseInt(hex2.substring(0, 2), 16);
     var g2 = parseInt(hex2.substring(2, 4), 16);
     var b2 = parseInt(hex2.substring(4, 6), 16);
+
     // calculate differences between reds, greens and blues
     var r = 255 - Math.abs(r1 - r2);
     var g = 255 - Math.abs(g1 - g2);
     var b = 255 - Math.abs(b1 - b2);
+
     // limit differences between 0 and 1
     r /= 255;
     g /= 255;
     b /= 255;
+
     // 0 means opposit colors, 1 means same colors
     return (r + g + b) / 3;
 };
